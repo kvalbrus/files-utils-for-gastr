fd9deea28171a247cf4b53e2c58adb2e5230ac02
kvalbrus <kvalbrus@gmail.com>
2024-03-12 20:10:25 +0300

    Add logging

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,17 @@ public class Main {
         final var str1 = new ArrayList<>(new BufferedReader(new FileReader("a")).lines().toList());
         final var str2 = new ArrayList<>(new BufferedReader(new FileReader("b")).lines().toList());
 
+        System.out.println("Перебирание:");
+
+        int i = 0;
+//        int h = str2.size() / 100;
         for (final var s2 : str2) {
+            if (i % 100000 == 0) {
+                System.out.println(i + "/" + str2.size());
+            }
+
+            i++;
+
             str1.remove(s2);
         }
 
@@ -15,13 +25,23 @@ public class Main {
 
         BufferedWriter writer = new BufferedWriter(new FileWriter("c"));
 
-        str1.forEach(it -> {
+        System.out.println("Создание файла:");
+
+        int j = 0;
+//        h = str1.size() / 100;
+        for (final var s1 : str1) {
             try {
-                writer.write(it + "\n");
+                if (j % 100000 == 0) {
+                    System.out.println(j + "/" + str1.size());
+                }
+
+                j++;
+
+                writer.write(s1 + "\n");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        });
+        }
 
         writer.close();
     }
